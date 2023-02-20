b85f56ab4ec1f332b9e173da375387cbf437384f
Fabian Munoz <fabianfa.munoz@gmail.com>
2023-02-20 15:47:12 -0500

    Implementado midelware control de roles

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { login, register} from "../controllers/userController.js"
 import verifyToken from "../middelware/validateToken.js"
+import validateRole from "../middelware/validateRole.js";
+import validateToken from "../middelware/validateToken.js";
 
 const router = new Router();
 
 router.post('/login', login);
 router.post('/register', register);
-router.get('/profile', verifyToken);
+router.get('/profile', validateToken, validateRole(["USER"]));
 router.put('/profile');
 router.delete('/profile');
 
