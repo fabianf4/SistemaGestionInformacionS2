99b52cfbc275949945a963becc634e3678e724fa
Fabian Munoz <fabianfa.munoz@gmail.com>
2023-02-24 15:14:24 -0500

    Agregado modelo de solicitud de certificado y agregar solicitud

---
+++ src/models/requestCertificateModel.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize"
+import sequalize from "../config/mysqlConfig.js"
+
+const requestCertificateModel = sequalize.define("RequestCertificate", {
+    userUuid: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    book: {
+        type: DataTypes.TINYINT,
+        allowNull: false
+    },
+    invoice: {
+        type: DataTypes.SMALLINT,
+        allowNull: false
+    },
+    number: {
+        type: DataTypes.SMALLINT,
+        allowNull: false
+    },
+    orderDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    alter: false //update table if exists
+})
+
+userModel.sync()
+
+export default requestCertificateModel
