6e846f9b1cd9625037a182787c2887674259b9ef
Harold_Herrea <harold.herrera@uptc.edu.co>
2023-03-06 09:19:29 -0500

    marriage controller

--- src/controllers/marriageController.js
+++ src/controllers/marriageController.js
@@ -75,3 +75,130 @@ export async function addMarriage(req, res) {
         })
     }
 }
+
+export async function getMarriageToNameLastname(req, res){
+    const {name, lastname}= req.body
+    try{
+        const marriage =await marriageCertificateModel.findAll({
+            where: {name, lastname}
+        })
+        if(!marriage || marriage.length===0){
+            return res.status(200).json({
+                success: false,
+                message: "Acta(s) no encontradas"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            data:{
+                marriage
+            },
+            message: "Acta(s) encontrada"
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error trayendo actas"
+        })
+    }
+}
+
+
+
+export async function deleteMarriage(req, res){
+    try{  
+        
+        const {book, invoice, number}= req.body
+        const marriage =await findOneMarriage(book, invoice, number)
+
+        if(!marriage){
+            return res.status(200).json({
+            success: false,
+            message: 'Registro no encontrado'
+            });
+        }
+        await marriage.destroy();
+
+        return res.status(200).json({
+            success: true,
+            message: 'Registro eliminado exitosamente'
+        });
+        
+    } catch(err) {
+        return res.status(500).json({
+          success: false,
+          message: 'Error eliminando registro de Matrimono'
+        });
+      }
+    
+}
+ 
+
+export async function updateMarriage(req, res) {
+    const {
+        book,
+        invoice,
+        number,
+        marrierdate,
+        namehusband,
+        lastnamehusband,
+        fatherhusband,
+        motherhusband,
+        placebatptismhusband,
+        datebatptismhusband,
+        namewife,
+        lastnamewife,
+        fatherwife,
+        motherwife,
+        placebatptismwife,
+        datebatptismwife,
+        namewitness,
+        minister,
+        parson,
+        annotations
+    } = req.body
+     try {
+        const marriage = await marriageCertificateModel.findOne({where: {book, invoice, number}});
+        if(!marriage) {
+            return res.status(200).json({
+                success: false,
+                message: "Acta de Confirmacion no encontrada"
+            });
+        }
+
+        await marriage.update({
+            book,
+            invoice,
+            number,
+            marrierdate,
+            namehusband,
+            lastnamehusband,
+            fatherhusband,
+            motherhusband,
+            placebatptismhusband,
+            datebatptismhusband,
+            namewife,
+            lastnamewife,
+            fatherwife,
+            motherwife,
+            placebatptismwife,
+            datebatptismwife,
+            namewitness,
+            minister,
+            parson,
+            annotations
+        })
+        return res.status(200).json({
+            success: true,
+            data: {
+                marriage
+            },
+            message: "Acta de Matrimonio actualizada exitosamente"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error actualizando acta de Matrimonio"
+        });
+    }
+}
