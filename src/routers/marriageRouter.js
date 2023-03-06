4b711e54c7281059d9ad80a4280f3155517c50a7
Harold_Herrea <harold.herrera@uptc.edu.co>
2023-03-06 09:25:40 -0500

    router Marriage

--- src/routers/marriageRouter.js
+++ src/routers/marriageRouter.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
-import {addMarriage} from "../controllers/marriageController.js"
+import {addMarriage,getMarriageToNameLastname,deleteMarriage,updateMarriage} from "../controllers/marriageController.js"
 const router= new Router()
 
 router.post('/addMarriage',addMarriage)
+router.get('/getMarriageToNameLastname',getMarriageToNameLastname)
+router.delete('/deleteMarriage',deleteMarriage)
+router.put('/updateMarriage',updateMarriage)
 
 export default router
