7d0dd5b90f64298f9d380e0dfb5911cfbde0def9
Fabian Munoz <fabianfa.munoz@gmail.com>
2023-03-08 08:42:10 -0500

    Agregado validaciones de token y role a bautismo, confirmacion y usuario

--- src/routers/baptismRouter.js
+++ src/routers/baptismRouter.js
@@ -8,15 +8,37 @@ import {
 import addUpdateBaptismValidator from "../middlewares/baptismCertificateValidators/addUpdateBaptismValidator.js"
 import deleteBaptismValidator from "../middlewares/baptismCertificateValidators/deleteBaptismValidator.js"
 import getBaptismToNameLastnameValidator from "../middlewares/baptismCertificateValidators/getBaptismToNameLastnameValidator.js"
+import validateRole from "../middlewares/validateRole.js"
+import validateToken from "../middlewares/validateToken.js"
 
 const router = new Router()
 
-router.post("/addBaptism", addUpdateBaptismValidator, addBaptism)
+router.post(
+    "/addBaptism",
+    validateToken,
+    validateRole(["ADMIN"]),
+    addUpdateBaptismValidator,
+    addBaptism
+)
 router.get(
     "/getBaptismToNameLastname",
+    validateToken,
+    validateRole(["ADMIN"]),
     getBaptismToNameLastnameValidator,
     getBaptismToNameLastname
 )
-router.delete("/deleteBaptism", deleteBaptismValidator, deleteBaptism)
-router.put("/updateBaptism", getBaptismToNameLastnameValidator, updateBaptism)
+router.delete(
+    "/deleteBaptism",
+    validateToken,
+    validateRole(["ADMIN"]),
+    deleteBaptismValidator,
+    deleteBaptism
+)
+router.put(
+    "/updateBaptism",
+    validateToken,
+    validateRole(["ADMIN"]),
+    getBaptismToNameLastnameValidator,
+    updateBaptism
+)
 export default router
--- src/routers/confirmationRouter.js
+++ src/routers/confirmationRouter.js
@@ -1,15 +1,44 @@
 import { Router } from "express"
-import {addConfirmation, getConfirmToNameLastname, deleteConfirmation, updateConfirmation} from "../controllers/confirmationController.js"
-const router= new Router()
+import {
+    addConfirmation,
+    getConfirmToNameLastname,
+    deleteConfirmation,
+    updateConfirmation
+} from "../controllers/confirmationController.js"
+const router = new Router()
 import addUpdateConfirmationValidator from "../middlewares/confirmationCertificateValidators/addUpdateConfirmationValidator.js"
 import deleteConfirmationValidator from "../middlewares/confirmationCertificateValidators/deleteConfirmationValidator.js"
 import getConfirmationToNameLastnameValidator from "../middlewares/confirmationCertificateValidators/getConfirmationToNameLastnameValidator.js"
 import validateToken from "../middlewares/validateToken.js"
 import validateRole from "../middlewares/validateRole.js"
 
-router.post('/addConfirmation', validateToken,validateRole(["ADMIN"]), addUpdateConfirmationValidator ,addConfirmation)
-router.get('/getConfirmToNameLastname', validateToken, getConfirmationToNameLastnameValidator, getConfirmToNameLastname)
-router.delete('/deleteConfirmation', deleteConfirmationValidator, deleteConfirmation)
-router.put('/updateConfirmation', addUpdateConfirmationValidator,updateConfirmation)
+router.post(
+    "/addConfirmation",
+    validateToken,
+    validateRole(["ADMIN"]),
+    addUpdateConfirmationValidator,
+    addConfirmation
+)
+router.get(
+    "/getConfirmToNameLastname",
+    validateToken,
+    validateRole(["ADMIN"]),
+    getConfirmationToNameLastnameValidator,
+    getConfirmToNameLastname
+)
+router.delete(
+    "/deleteConfirmation",
+    validateToken,
+    validateRole(["ADMIN"]),
+    deleteConfirmationValidator,
+    deleteConfirmation
+)
+router.put(
+    "/updateConfirmation",
+    validateToken,
+    validateRole(["ADMIN"]),
+    addUpdateConfirmationValidator,
+    updateConfirmation
+)
 
 export default router
